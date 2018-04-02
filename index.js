ae69ac5a6602272b7754d402047b7722d393db9a
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-04-02 11:11:02 -0400

    adds nexttick code

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-// node has a global object that works similarly to the browser's window object
-// there are some differences though
+/*
+ * The process.nextTick() method adds the callback to the "next tick queue". 
+ * Once the current turn of the event loop turn runs to completion, 
+ * all callbacks currently in the next tick queue will be called.
+ * This is not a simple alias to setTimeout(fn, 0). 
+ * It is much more efficient. It runs before any additional I/O events (including timers) 
+ * fire in subsequent ticks of the event loop.
+ * https://nodejs.org/api/process.html#process_process_nexttick_callback_args
+ */
 
-let name = 'Josh';
-console.log(name); // Josh
+// functions
+let logger = (param) => {
+  param = param || 'No arg passed';
+  console.log(param);
+};
 
-// undefined -- whereas on regular js (on the browser) window.name would be 'Josh'
-// this is because 'name' is scoped to this module (this file) and are not added to the global object like in the browser
-console.log(global.name); 
+let getDirections = (location) => {
+  if (location === 'home') { 
+    logger('Go straight'); 
+  }
 
-// if we want to add a variable to the global object we need to do it explicitly
-global.name = 'Josh';
+  if (location === 'downtown') { 
+    process.nextTick(() => logger('Make a right at the light')); 
+  }  
 
-console.log(global.name); // Josh
+  if (location === 'uptown') { 
+    setTimeout(() => logger('Make a left at the light'), 0);
+  }
 
-// NOTE: we can use most js methods in node, such as substring, slice, splice, etc
-console.log(name.slice(1)); // osh
+};
 
+let askDirections = (location, cb) => cb(location);
 
-// THERE ARE OTHER GLOBAL OBJECTS YOU SHOULD KNOW ABOUT 
-// BELOW ARE SOME IMPORTANT ONES. FOR A COMPLETE LIST, FOLLOW THIS LINK: https://nodejs.org/api/globals.html
+// logging
+logger('Start'); // 1
 
-console.log(__dirname); // the path to the directory name of the current module
-console.log(__filename); // the path to the filename of the current module
-console.log(exports); // short for module.exports -- allows you to export modules (that can then be imported on other modules)
-// console.log(require());// allows you to import modules that have been exported -- need the module path passed as argument
-console.log(process);// gives you access to the current node process
+setTimeout(() => logger('sto 1'), 0); // 6
+
+setTimeout(() => logger('sto 2'), 0); // 7
+
+process.nextTick(() => logger('Next tick')); // 4 -- runs before setTimeout
+
+askDirections('home', getDirections); // 2
+
+askDirections('downtown', getDirections); // 5 -- calls 'process.nextTick' in the 'if' block
+
+askDirections('uptown', getDirections); // 8 -- calls 'setTimeout' in the 'if' block; runs after all other events are finished
+
+logger('End'); // 3
+
+/*
+Prints:
+  1.Start
+  2.Go straight
+  3.End
+  4.Next tick
+  5.Make a right at the light
+  6.sto 1
+  7.sto 2
+  8.Make a left at the light
+*/
+
