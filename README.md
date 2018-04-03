a4bdf3d1920106723ca74e993362d43c0d61629e
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-04-03 12:07:32 -0400

    adds code

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-# node-tutorial-from-scratch
+# Events [https://nodejs.org/api/events.html](https://nodejs.org/api/events.html)
 
-A node tutorial from the ground up, covering core concepts of node.js, node-express servers, best practices, and design patterns.
+Much of the Node.js core API is built around an idiomatic asynchronous event-driven architecture in which certain kinds of objects (called "emitters") periodically emit named events that cause Function objects ("listeners") to be called.
 
-Each branch covers a different topic, specified by the branch name. If you are a beginner, start from the first branch. If you are a node developer who wants to revise a topic, just checkout to the branch of your interest.
+All objects that emit events are instances of the EventEmitter class. These objects expose an eventEmitter.on() function that allows one or more functions to be attached to named events emitted by the object. Typically, event names are camel-cased strings but any valid JavaScript property key can be used.
+
+When the EventEmitter object emits an event, all of the functions attached to that specific event are called synchronously. Any values returned by the called listeners are ignored and will be discarded.
+
+i.e. Simple emmiter
+
+```javascript
+  const EventEmitter = require('events');
+  
+  // class pattern
+  class MyEmitter extends EventEmitter {}
+  
+  // variable pattern 
+  const myEmitter = new MyEmitter();
+  
+  // set the listener
+  myEmitter.on('event', () => {
+    console.log('an event occurred!');
+  });
+  
+  // emit the event
+  myEmitter.emit('event');
+```  
+
+
---
+++ index.js
@@ -0,0 +1,58 @@
+const events = require('events');
+
+// EventEmitter is a constructor
+const eventEmitter = new events.EventEmitter();
+
+// listenerCount is a function
+const eventEmitterListenerCount = require('events').EventEmitter.listenerCount;
+
+// listener #1
+const listener1 = () => console.log('listener1 executed.');
+
+// listener #2
+const listener2 = () => console.log('listener2 executed.');
+
+// Bind a custom 'connection' event with the listener1 function
+eventEmitter.on('connection', listener1);
+
+// Bind the connection event with the listener2 function(this is an alias to '.on')
+eventEmitter.addListener('connection', listener2);
+
+let eventListeners = eventEmitterListenerCount(eventEmitter,'connection');
+
+console.log(eventListeners + " Listener(s) listening to connection event");
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+// Remove the binding of 'listener1' function
+eventEmitter.removeListener('connection', listener1);
+console.log("Listener1 will not listen now.");
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+eventListeners = eventEmitterListenerCount(eventEmitter,'connection');
+console.log(eventListeners + " Listener(s) listening to connection event");
+
+// Remove the binding of 'listener1' function
+eventEmitter.removeListener('connection', listener2);
+console.log("Listener2 will not listen now.");
+
+eventListeners = eventEmitterListenerCount(eventEmitter,'connection');
+console.log(eventListeners + " Listener(s) listening to connection event");
+
+console.log("Program Ended.");
+
+/*
+  Prints:
+    2 Listener(s) listening to connection event
+    listener1 executed.
+    listener2 executed.
+    Listener1 will not listen now.
+    listener2 executed.
+    1 Listener(s) listening to connection event
+    Listener2 will not listen now.
+    0 Listener(s) listening to connection event
+    Program Ended.
+*/
