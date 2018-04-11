d563f17190cf00bb548cbc1ec39fd353379ee4b8
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-04-11 17:13:53 -0400

    implements disconnect event; implements io.emit to broadcast to all users instead of socket.broadcast.emit

--- public/js/sio-client.js
+++ public/js/sio-client.js
@@ -5,10 +5,10 @@
  */
 const socket = io('http://localhost:3000'); 
 
-// on disconnect -- this is a native socket.io event
+// on disconnect -- this is a native socket.io event; it is reserved to the socket.io api
 socket.on('disconnect', () => setTitle('Disconnected from chat'));
 
-// on connect -- this is a native socket.io event
+// on connect -- this is a native socket.io event; it is reserved to the socket.io api
 socket.on('connect', () => {
   // emit event to the server informing this client is connected
   socket.emit('client-message', 'Client connected');
@@ -27,7 +27,10 @@ document.forms[0].onsubmit = () => {
   if (value === 'exit') {
     socket.emit('disconnect', value);
   } else {
-    printMessage(value); // prints the message right away
+    // prints the message right away in case we use
+    // socket.broadcast.emit('message', message) on the server side 
+    // instead of io.emit('message', message) -- see comment on sio-server.io
+    // printMessage(value); 
 
     // when the user sends a message on the form we
     // need to use socket.io to emit that event to the server, 
