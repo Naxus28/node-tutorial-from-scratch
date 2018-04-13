b16869b46193a612cfd89b8e1a857e9e8f3e122e
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-04-13 10:50:55 -0400

    adds code and comments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# test with mocha [mocha](https://mochajs.org/) and [chai](http://www.chaijs.com/)
+# mocking web servers with [nock](https://github.com/node-nock/nock)
 
 Mocha gives us a way to describe, run, and build tests but it doesn't have a mechanism to check values. We can use Chai along with Mocha to be able to check values.
 Mocha should be installed globally `npm i -g mocha` so we can run it in any project from the command line as such `mocha <test directory> or <test file>`. Chai should be installed per project as a dev dependency `npm i -D chai`.
--- test/tools.spec.js
+++ test/tools.spec.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai'); // can also use "should and assert functions"
 const tools = require('../lib/tools');
+const nock = require('nock');
 
 // describe functions can be nested
 
@@ -14,18 +15,21 @@ describe('Tools testing suite', () => {
   }); 
   
    describe('getWiki()', () => {
-    it('should load George Boole WikiPedia page', (done) => {
 
-      // This function runs asynchronous code
-      // so mocha needs to know that the code has not
-      // finished executing before it asserts it.
-      // That is why we use 'done': mocha waits until this function is called
-      // to assert the test. 
-      // The default wait time is 2000ms (it can be customized).
-      // If the 'done' function is not called mocha fails
-      tools.loadWiki({first: 'George', last: 'Boole'}, (html) => {
-        expect(html).to.be.ok;
-        done();
+    // before hook: this will run before any test on this suite
+    before(() => {
+      // this line mocks a get request to https://en.wikipedia.org/wiki/George_Boole
+      nock('https://en.wikipedia.org')
+        .get('/wiki/George_Boole')
+        .reply(200, 'Mock George Boole Wiki Page');
+    });
+
+    // tools.loadWiki will hit our mock server instead of the read wikipedia server
+    // since we are not hitting the external service, there is no latency, 
+    // so we can safely remove the 'done' function
+    it('should load George Boole WikiPedia page', () => {
+      tools.loadWiki({first: 'George', last: 'Boole'}, (mockContent) => {
+        expect(mockContent).to.equal('Mock George Boole Wiki Page');
       })
     });
   });
