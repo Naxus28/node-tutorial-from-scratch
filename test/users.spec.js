80884f131008216f2b3dd345d80717f0116b312f
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-04-16 09:45:28 -0400

    comments out tests that are not using spy

--- test/users.spec.js
+++ test/users.spec.js
@@ -42,23 +42,22 @@ describe('Users', function() {
       // about the calls to countBy: http://sinonjs.org/releases/v4.5.0/spies/ (check 'Properties')
       this.spy = sinon.spy(users.countBy);
 
-      // call the spy twice so we 
-      // can then get information about the calls later
+      // call the spy so we can then get information about the calls later
       this.spy({gender: 'Male', country: 'BR'});
       this.spy({gender: 'Male'});
 
-      users.__set__('users', this.users); 
+      // users.__set__('users', this.users); 
     });
 
-    it('should count 3 females', () => {
+    // it('should count 3 females', () => {
       // we have 3 female users in the mocked array above
-      expect(users.countBy({gender: 'Female'})).to.equal(3);
-    });
+      // expect(users.countBy({gender: 'Female'})).to.equal(3);
+    // });
 
-    it('should count 2 males', () => {
+    // it('should count 2 males', () => {
       // we have 2 males from Hmong in the mocked array above
-      expect(users.countBy({gender: 'Male', nationality: 'Hmong'})).to.equal(2);
-    });
+    //   expect(users.countBy({gender: 'Male', nationality: 'Hmong'})).to.equal(2);
+    // });
 
     // get information about the spy calls
     it('should call countBy 2 times', () => {
@@ -66,7 +65,7 @@ describe('Users', function() {
       // we need to get the spy function created in  'beforeEach'
       // from 'this.ctx' as opposed to 'this' (console.log(this) to see what it looks like)
       expect(this.ctx.spy.callCount).to.equal(2);  
-    }); // get information about the calls
+    }); 
     
 
     it('should call countBy twice--check with calledTwice', () => {
