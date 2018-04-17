2ed31562b921b389dacaa3ee7cf5981f43fc0a84
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-04-17 11:44:05 -0400

    adds data.js and styles.css

---
+++ server/data.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    term: 'JS Closures',
+    definition: 'A closure is the combination of a function and the lexical environment within which that function was declared.'
+  },
+  {
+    term: 'JS Prototype',
+    definition: 'Prototypes are the mechanism by which JavaScript objects inherit features from one another. In this article, we explain how prototype chains work and look at how the prototype property can be used to add methods to existing constructors.'
+  },
+  {
+    term: 'JS Bind function',
+    definition: 'The bind() method creates a new function that, when called, has its this keyword set to the provided value, with a given sequence of arguments preceding any provided when the new function is called'
+  },
+];
