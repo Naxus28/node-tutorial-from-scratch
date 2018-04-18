534aa535fa4f052f0906c1610450f22b76686a0c
Gabriel Ferraz <gabrielferraz27@gmail.com>
2018-04-18 16:19:00 -0400

    adds code and comments

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
-# endpoint that sends data to the client
+# use express.static() to send static files [https://expressjs.com/en/starter/static-files.html](https://expressjs.com/en/starter/static-files.html)
 
-The `res` object has different methods that allow us to send data to the client.
-In this demo we use `res.json(data)` to send json back to the client.
+When serving static files such as images, javascript, css, etc, we need to tell express to do so by using `express.static()`.
+
+
+`express.static()` takes the path to a directory or static file. 
+
+e.g
+```javascript
+express.static('public'); // serves all static files in the public directory and subdirectories
+```
+
+To use `express.static()` we need to create a middleware to our app (`app.use()`) and pass `express.static('directory')` as its argument:
+
+e.g.
+```javascript
+import express from 'express';
+const app = express();
+
+// the "use method" creates the middleware
+// route defaults the to '/' if no route is passed as first param
+app.use(express.static('public')); // serves all static files from /public on the '/' route
+
+// we can pass a route as the first argument to the middleware--the middleware is triggered only on that route
+app.use('/images', express.static('images')); //serves the images directory on the /images route
+```
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -3,10 +3,36 @@ import users from './data/users';
 const app = express();
 const PORT = 3000;
 
+/* middleware need to come before the route
+* serves static content from the public directory
+* on the default route "/" (because route was not specified as first param)
+* this will serve html, js, css, and img files
+* so when we navigate to http://localhost:3000 
+* express will serve index.html and all files referenced on this page will 
+* load as well, as long as they are hosted in /public
+* we can open them individually as well as such:
+* 1. 'http://localhost:3000/css/styles.css'
+* 2. 'http://localhost:3000/js/index.js'
+* 2. 'http://localhost:3000/img/node-express.png'
+*/
+app.use(express.static('public'));
+
+
+/* serves the random imgs directory on the /random-imgs route
+ * try 'http://localhost:3000/random-imgs/random1.gif'
+ * try 'http://localhost:3000/random-imgs/Random.png'
+ * ...
+ */
+app.use('/random-imgs', express.static('random-imgs'));
+
+
 // get methods are called on page load
 app.get('/', (req, res) => {
   console.log(`A get request on ${req.url}`);
-  res.json(users);
+  // because we are serving static files on the '/' route
+  // this api will only send this data to ajax GET requests to '/'
+  // but not on page load anymore
+  res.send(users); 
 });
 
 // need to send a post request to trigger this route
